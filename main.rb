6dd02f3ae621c6104d45021f4dfa970ab89fe826
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2021-01-12 10:14:15 +0000

    Interpolation

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
-name = 'Marie'
-# This will just output Tash
-puts name
-#name = "Tash"
-#This will output Tash & 20 on separate lines
-age = 20
-#Integers do not have "" on them this is only string
-puts name
-puts age
-#
+puts "Interpolation works in double quoted strings: #{1 + 2}."
+puts 'And it does not work in single quoted strings: #{1 + 2}.'
+price = 3.75
+quantity = 4
+total = price * quantity
+puts "the total cost is #{total}!"
+#interpolation allows us to insert almost anything into strings. Needs to be with double quotes though ""
+#another example
+shoes = "sandals"
+wear = "I wear #{1+1} #{shoes}"
+print wear
+#always double check the quotation marks (you may need to retype to ensure it is seen as a string etc)
