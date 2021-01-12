aa56bbdf7698bf0dcc298d95fe2b9f504c6f343c
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2021-01-12 10:15:54 +0000

    HLT Practice

--- main.rb
+++ main.rb
@@ -1,12 +1,16 @@
-puts "Interpolation works in double quoted strings: #{1 + 2}."
-puts 'And it does not work in single quoted strings: #{1 + 2}.'
-price = 3.75
-quantity = 4
-total = price * quantity
-puts "the total cost is #{total}!"
-#interpolation allows us to insert almost anything into strings. Needs to be with double quotes though ""
-#another example
-shoes = "sandals"
-wear = "I wear #{1+1} #{shoes}"
-print wear
-#always double check the quotation marks (you may need to retype to ensure it is seen as a string etc)
+name = ""
+years = 0
+MONTHS_PER_YEAR = 12 # a constant
+# output variable
+
+# processing
+print "What is your name? "
+name = gets
+
+print"How old are you? "
+years = gets
+
+months = years * MONTHS_PER_YEAR
+
+puts"#{name}, at #{years} years old, "\
+"you are #{months} months old."
