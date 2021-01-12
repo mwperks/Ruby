c958cbad410430cea2bfa19c8603915eeac9f1ca
Marie Wilcox-Perks <marie.perks@hotmail.co.uk>
2021-01-12 10:11:18 +0000

    new code

--- main.rb
+++ main.rb
@@ -1,6 +1,13 @@
-puts 3+2
-puts 5-1
-puts 3/2
-puts 6*6
-puts 5 ** 5
-puts 35 % 5
+#modulo operator. It gives the remainder when counter is divided by 2.
+
+puts 3 % 2 #1 due to having 1 left over
+
+puts 2 % 2 #0 due to dividing and having none remaining
+
+puts 23 % 3 #2 due to having 2 left over
+
+puts 89 % 9 #8 due to having 8 left over
+
+#So if you divided a number by two, and the integer remainder of that number is one, then you know the number was odd. Your example checks for odd numbers.
+
+#Often this is done to highlight odd number rows with a different background color, making it easier to read large lists of data. 
